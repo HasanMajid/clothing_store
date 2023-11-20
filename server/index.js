60064e597ae7d47248bb61c8a21524e2f9fe30fa
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-11-20 12:39:38 -0500

    Co-authored-by: MarkBelleza <MarkBelleza@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -44,14 +44,21 @@ app.use(cors(corsOption));
 //if you want in every domain then
 app.use(cors());
 
-app.get("/", async (req, res) => {
-  const user = await runQuery("SELECT * FROM usr");
-  res.send(user[0]);
+// app.get("/", async (req, res) => {
+//   const user = await runQuery("SELECT * FROM usr");
+//   res.send(user[0]);
+// });
+
+app.get("/getUsers", async (req, res) => {
+  const users = await runQuery("SELECT * FROM usr");
+  res.send(users);
 });
 
-app.get("/getUser", async (req, res) => {
-  const user = await runQuery("SELECT * FROM usr");
-  res.send(user[0]);
+app.get("/populate", async (req, res) => {
+  const response = await runQuery(
+    "INSERT ALL INTO usr VALUES ('usr1', 'employee', 'John', 'David', 'john@example.com', '123-456-7890', '03161970') INTO usr VALUES ('usr2', 'employee', 'James', 'Poe', 'james@example.com', '987-654-3210', '01141997') INTO usr VALUES ('usr3', 'customer', 'Kevin', 'Kim', 'Kevin@example.com', '123-446-8888', '04061991') INTO usr VALUES ('usr4', 'customer', 'Brian', 'Pham', 'Brian@example.com', '987-612-3420', '12141984') INTO usr VALUES ('usr5', 'admin', 'Victoria', 'Lee', 'Victoria@example.com', '123-152-8312', '10161999') INTO usr VALUES ('usr6', 'admin', 'Joseph', 'Smith', 'Joseph@example.com', '987-651-1527', '12032001') SELECT 1 FROM DUAL"
+  );
+  res.send(response);
 });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
