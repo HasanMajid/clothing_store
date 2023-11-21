067b2b98c777f53214876ba429dba5126f0be2d8
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-11-20 22:35:41 -0500

    deleted comment

--- server/index.js
+++ server/index.js
@@ -61,9 +61,6 @@ app.get("/populateUsers", async (req, res) => {
   await runQuery(
     "INSERT ALL INTO usr VALUES ('usr1', 'employee', 'John', 'David', 'john@example.com', '123-456-7890', '03161970') INTO usr VALUES ('usr2', 'employee', 'James', 'Poe', 'james@example.com', '987-654-3210', '01141997') INTO usr VALUES ('usr3', 'customer', 'Kevin', 'Kim', 'Kevin@example.com', '123-446-8888', '04061991') INTO usr VALUES ('usr4', 'customer', 'Brian', 'Pham', 'Brian@example.com', '987-612-3420', '12141984') INTO usr VALUES ('usr5', 'admin', 'Victoria', 'Lee', 'Victoria@example.com', '123-152-8312', '10161999') INTO usr VALUES ('usr6', 'admin', 'Joseph', 'Smith', 'Joseph@example.com', '987-651-1527', '12032001') SELECT 1 FROM DUAL"
   );
-  // await runQuery(
-  //   "INSERT INTO usr VALUES ('usr1', 'employee', 'John', 'David', 'john@example.com', '123-456-7890', '03161970')"
-  // );
   res.send([{ message: "populated table" }]);
 });
 
